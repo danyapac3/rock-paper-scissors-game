aed3ef833944f08bafbb234a443c8d6e35744138
danya.pac3 <danya.pac3@gmail.com>
2025-02-13 16:08:16 +0200

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('hello world');
+function getComputerChoice() {
+  const choice = Math.floor(Math.random() * 3);
+  switch(choice) {
+    case 0: return 'rock';
+    case 1: return 'paper';
+    case 2: return 'scissors';
+  }
+}
